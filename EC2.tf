534101380fc56793c302bc2cd8e96b449e73285b
RealAbhishekPY <abhishekambulkar2017@gmail.com>
2025-03-06 17:05:54 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -11,12 +11,17 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"  # Specifies the AWS region
-  profile = "new.profile.name"  # profile name 
+  profile = "default"  # profile name 
 }
 
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a"  # Specifies the Amazon Machine Image (AMI) ID
   instance_type = "t2.micro"  # Specifies the instance type
+  ebs_block_device {
+    device_name = "/dev/sdh"  # Specifies the device name
+    volume_size = 20  # Specifies the size of the volume in GB
+    volume_type = "gp2"  # Specifies the type of the volume (e.g., gp2, io1, etc.)
+  }
 
   tags = {
     Name = "NewTagName"  #tag name
