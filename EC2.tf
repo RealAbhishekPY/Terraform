389e075a56d1abad5917a8a9e4d25edd97a80b8d
RealAbhishekPY <abhishekambulkar2017@gmail.com>
2025-03-06 17:01:14 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -2,23 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.16"  # Specifies the AWS provider version
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"  # Specifies the required Terraform version
 }
 
 provider "aws" {
-  region  = "us-west-2"
-  profile = "jack.roper"
+  region  = "us-west-2"  # Specifies the AWS region
+  profile = "new.profile.name"  # profile name 
 }
 
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
-  instance_type = "t2.micro"
+  ami           = "ami-04e914639d0cca79a"  # Specifies the Amazon Machine Image (AMI) ID
+  instance_type = "t2.micro"  # Specifies the instance type
 
   tags = {
-    Name = "JacksBlogExample"
+    Name = "NewTagName"  #tag name
   }
 }
