10e0c14657db19f34a97676f021b7f9c741a68ad
Abhishek Ambulkar <abhishekambulkar2017@gmail.com>
2025-03-07 14:05:32 +0530

    Create 10_cloudwatch.tf

---
+++ 10_cloudwatch.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high-cpu-utilization"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  dimensions = {
+    InstanceId = aws_instance.web.id
+  }
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+}
